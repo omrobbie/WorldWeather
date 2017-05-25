f5670e4dd3de51affd79e0a19ced93ead93498c6
omrobbie <putu.roby@yahoo.com>
2017-05-25 21:58:08 +0800

    tambahkan fungsi onBindViewHolder

--- app/src/main/java/com/omrobbie/worldweather/WeatherAdapter.java
+++ app/src/main/java/com/omrobbie/worldweather/WeatherAdapter.java
@@ -5,9 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
 
@@ -29,7 +31,7 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
      * layout file.
      * <p>
      * The new ViewHolder will be used to display items of the adapter using
-     * {@link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+     * {@link #onBindViewHolder(ViewHolder, int)}. Since it will be re-used to display
      * different items in the data set, it is a good idea to cache references to sub views of
      * the View to avoid unnecessary {@link View#findViewById(int)} calls.
      *
@@ -47,12 +49,53 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         return viewHolder;
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method
+     * should update the contents of the {@link ViewHolder#itemView} to reflect the item at
+     * the given position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Partial bind vs full bind:
+     * <p>
+     * The payloads parameter is a merge list from {@link #notifyItemChanged(int, Object)} or
+     * {@link #notifyItemRangeChanged(int, int, Object)}.  If the payloads list is not empty,
+     * the ViewHolder is currently bound to old data and Adapter may run an efficient partial
+     * update using the payload info.  If the payload is empty,  Adapter must run a full bind.
+     * Adapter should not assume that the payload passed in notify methods will be received by
+     * onBindViewHolder().  For example when the view is not attached to the screen, the
+     * payload in notifyItemChange() will be simply dropped.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     /* setup inner class view untuk inflater listitem layout */
-    private class ViewHolder {
+    private class ViewHolder extends RecyclerView.ViewHolder {
 
         /* setup constructor untuk inner class ViewHolder */
-        public ViewHolder(View view) {
-            super();
+        public ViewHolder(View itemView) {
+            super(itemView);
         }
     }
 }
